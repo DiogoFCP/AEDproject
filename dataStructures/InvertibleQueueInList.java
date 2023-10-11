db72f864890693618b907e9bebed2eb3bc498ab7
idgafox <t.r.cosata2012@gmail.com>
2023-10-11 10:05:53 +0100

    InvertibleQueueInList done

--- dataStructures/InvertibleQueueInList.java
+++ dataStructures/InvertibleQueueInList.java
@@ -1,7 +1,34 @@
 package dataStructures;
 
-public class InvertibleQueueInList<E> extends QueueInList<E> implements InvertibleQueue<E>{
-    public void invert(){
+public class InvertibleQueueInList<E> extends QueueInList<E> implements InvertibleQueue<E> {
 
+    static final long serialVersionUID = 0L;
+
+    boolean leftToRight;
+
+    public InvertibleQueueInList() {
+        super();
+        leftToRight = true;
+    }
+
+    public void invert() {
+        leftToRight = !leftToRight;
+    }
+
+    public void equeue(E element) {
+        if (leftToRight)
+            list.addLast(element);
+        else
+            list.addFirst(element);
+    }
+
+    public E dequeue() throws EmptyQueueException {
+        if (list.isEmpty())
+            throw new EmptyQueueException();
+
+        if (leftToRight)
+            return list.removeFirst();
+        else
+            return list.removeLast();
     }
 }
